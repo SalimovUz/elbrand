9db1cf134694bbb332628f11e5db18465df94111
SalimovUz <Salimofftolqin7@gmail.com>
2025-04-04 14:04:45 +0500

    Finally

--- app/page.js
+++ app/page.js
@@ -1,103 +1,134 @@
+"use client";
 import Image from "next/image";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import SendIcon from "@mui/icons-material/Send";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import NearMeIcon from "@mui/icons-material/NearMe";
+import PhoneIcon from "@mui/icons-material/Phone";
+import Judo from "../public/elbrandlogo.png";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="min-h-screen bg-gradient-to-b from-blue-200 via-blue-100 to-blue-100 flex flex-col items-center relative">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      {/* Header Section */}
+      <div className="text-center py-8">
+        <div className="w-32 h-32 mx-auto rounded-full shadow-md border-4 border-green-500 overflow-hidden">
+          <Image src={Judo} alt="Logo" className="w-full h-full object-cover" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+
+        <h1 className="text-5xl font-extrabold text-red-800 mt-4">
+          Elbrand
+          
+        </h1>
+
+        <p className="text-gray-800 mt-2 text-lg tracking-wide">
+          🚚O'zbekiston bo'ylab yetkazish xizmati mavjud💥
+        </p>
+      </div>
+
+      {/* Social Media Links */}
+      <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-sm space-y-6 border-2 border-red-200">
+        <h1 className="text-center text-xl font-semibold text-green-800">
+          Ijtimoiy tarmoqlarimiz!
+        </h1>
+        <a target="_blank"
+          href="https://t.me/elbrand_rasmiy"
+          className="flex items-center gap-4 p-3 rounded-lg hover:bg-blue-50 transition border"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+          <TelegramIcon className="text-blue-600 text-3xl" />
+          <div>
+            <h1 className="text-lg font-bold">Telegram</h1>
+            <p className="text-gray-600">@Elbrand_rasmiy</p>
+          </div>
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <a target="_blank"
+          href="https://www.instagram.com/elbrand_uzb"
+          className="flex items-center gap-4 p-3 rounded-lg hover:bg-pink-50 transition border"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
+          <InstagramIcon className="text-pink-500 text-3xl" />
+          <div>
+            <h1 className="text-lg font-bold">Instagram</h1>
+            <p className="text-gray-600">@Elbrand_uzb</p>
+          </div>
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <a target="_blank"
+          href="https://t.me/mom1noow"
+          className="flex items-center gap-4 p-3 rounded-lg hover:bg-blue-50 transition border"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          <SendIcon className="text-blue-500 text-3xl" />
+          <div>
+            <h1 className="text-lg font-bold">Telegram Admin</h1>
+            <p className="text-gray-600">@sadriddin_01_09</p>
+          </div>
         </a>
-      </footer>
+        {/* Phone Number */}
+        <div className="flex items-center gap-4 p-3 rounded-lg hover:bg-green-50 transition border">
+          <PhoneIcon className="text-green-500 text-3xl" />
+          <div>
+            <h1 className="text-lg font-bold">Bog'lanish</h1>
+            <p className="text-gray-600">+998 97 774 06 69</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Locations Section */}
+      <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-sm space-y-6 mt-10 mb-8 border-2 border-red-200">
+        <h1 className="text-center text-xl font-semibold text-green-800">
+          Manzil (Filiallarimiz)
+        </h1>
+        <a target="_blank"
+          href="https://maps.windows.com/?form=WNAMSH&collection=point.41.25249_69.322419_Point"
+          className="flex items-start gap-4 p-3 rounded-lg hover:bg-gray-50 transition border"
+        >
+          <LocationOnIcon className="text-red-500 text-3xl" />
+          <div>
+            <h1 className="text-lg font-bold">Qo'yliq Pochta filiali</h1>
+            <p className="text-gray-600">☎️Tel: +998 97 774 06 69</p>
+          </div>
+        </a>
+        <a target="_blank"
+          href="https://maps.windows.com/?form=WNAMSH&collection=point.41.254285_69.373977_Point"
+          className="flex items-start gap-4 p-3 rounded-lg hover:bg-gray-50 transition border"
+        >
+          <LocationOnIcon className="text-red-500 text-3xl" />
+          <div>
+            <h1 className="text-lg font-bold">Bektemir Vodnik filiali</h1>
+            <p className="text-gray-600">☎️Tel: +998 99 256 66 77</p>
+          </div>
+        </a>
+        <a target="_blank"
+          href="https://maps.windows.com/?form=WNAMSH&collection=point.41.334611_69.407222_Point"
+          className="flex items-start gap-4 p-3 rounded-lg hover:bg-gray-50 transition border"
+        >
+          <LocationOnIcon className="text-red-500 text-3xl" />
+          <div>
+            <h1 className="text-lg font-bold">Mirzo Ulug'bek filiali</h1>
+            <p className="text-gray-600">☎️Tel: +998 99 050 39 63</p>
+          </div>
+        </a>
+        <a target="_blank"
+          href="https://maps.windows.com/?form=WNAMSH&collection=point.41.221389_69.258789_Point"
+          className="flex items-start gap-4 p-3 rounded-lg hover:bg-gray-50 transition border"
+        >
+          <LocationOnIcon className="text-red-500 text-3xl" />
+          <div>
+            <h1 className="text-lg font-bold">Sergeli filiali</h1>
+            <p className="text-gray-600">☎️Tel: +998 97 707 25 97</p>
+          </div>
+        </a>
+        <a target="_blank"
+          href="https://t.me/elbrand_rasmiy"
+          className="flex items-start gap-4 p-3 rounded-lg hover:bg-gray-50 transition border"
+        >
+          <LocationOnIcon className="text-red-500 text-3xl" />
+          <div>
+            <h1 className="text-lg font-bold">Yunusobod filiali</h1>
+            <p className="text-gray-600">☎️Tel: +998 97 774 06 69</p>
+          </div>
+        </a>
+      </div>
     </div>
   );
 }
